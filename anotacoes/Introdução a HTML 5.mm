bbcb9ead602fb51ac61b21d295b700747f4e4ff1
jedi.bytecode <bytecode@uol.com.br>
2022-07-19 02:24:32 -0300

    inicio introducao ao html5

---
+++ anotacoes/Introdução a HTML 5.mm
@@ -0,0 +1,99 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Introdu&#xe7;&#xe3;o a HTML 5" FOLDED="false" ID="ID_1511968562" CREATED="1658206887384" MODIFIED="1658206914020" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="4" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Hist&#xf3;ria e estrutura b&#xe1;sica" POSITION="right" ID="ID_1395899393" CREATED="1658206937058" MODIFIED="1658206944837">
+<edge COLOR="#0000ff"/>
+<node TEXT="criado em 1991 por tim berners-lee" ID="ID_1237177868" CREATED="1658206984594" MODIFIED="1658207007404"/>
+<node TEXT="5 vers&#xf5;es" ID="ID_1435312702" CREATED="1658207016490" MODIFIED="1658207025061"/>
+<node TEXT="&#x169;ltima em 2014" ID="ID_1246757416" CREATED="1658207025418" MODIFIED="1658207030236"/>
+<node TEXT="elemento HTML" ID="ID_1232505643" CREATED="1658207042090" MODIFIED="1658207059387">
+<node TEXT="tag de abertura" ID="ID_483315089" CREATED="1658207059395" MODIFIED="1658207063317">
+<node TEXT="tipo de elemento" ID="ID_534143843" CREATED="1658207070210" MODIFIED="1658207093308"/>
+<node TEXT="atributo" ID="ID_1354235268" CREATED="1658207093626" MODIFIED="1658207099260"/>
+<node TEXT="Conte&#xfa;do" ID="ID_385299226" CREATED="1658207100730" MODIFIED="1658207104524"/>
+</node>
+<node TEXT="tag de fechamento" ID="ID_786633694" CREATED="1658207106841" MODIFIED="1658207111235"/>
+</node>
+<node TEXT="Estrutura b&#xe1;sica" ID="ID_1642145279" CREATED="1658207115825" MODIFIED="1658207121707">
+<node TEXT="DOCTYPE" ID="ID_1182502020" CREATED="1658207128033" MODIFIED="1658207134451"/>
+<node TEXT="html" ID="ID_1929612173" CREATED="1658207136193" MODIFIED="1658207138507"/>
+<node TEXT="head" ID="ID_1139745114" CREATED="1658207138809" MODIFIED="1658207145044">
+<node TEXT="meta" ID="ID_1546359486" CREATED="1658207146377" MODIFIED="1658207150980">
+<node TEXT="exemplo, charcode" ID="ID_1638504372" CREATED="1658207168313" MODIFIED="1658207176731"/>
+</node>
+<node TEXT="title" ID="ID_501475280" CREATED="1658207151705" MODIFIED="1658207156691"/>
+</node>
+<node TEXT="body" ID="ID_1222451021" CREATED="1658207181681" MODIFIED="1658207194594"/>
+</node>
+</node>
+<node TEXT="Sem&#xe2;ntica" POSITION="right" ID="ID_843442196" CREATED="1658206951218" MODIFIED="1658206956139">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="Principais elementos HTML" POSITION="right" ID="ID_889856703" CREATED="1658206956730" MODIFIED="1658206970556">
+<edge COLOR="#ff00ff"/>
+</node>
+</node>
+</map>
